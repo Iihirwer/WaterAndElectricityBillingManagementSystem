91e1961a9ffb3d5a214d0bc829da522cf4814f85
Iihirwer <ihirweraissa@gmail.com>
2026-02-26 12:38:36 +0200

    new changes

---
+++ src/main/java/electricity/com/waterandelectricitybillingmanagementsystem/WaterAndElectricityBillingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package electricity.com.waterandelectricitybillingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterAndElectricityBillingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterAndElectricityBillingManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/electricity/com/waterandelectricitybillingmanagementsystem/WaterAndElectricityBillingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package electricity.com.waterandelectricitybillingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterAndElectricityBillingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
